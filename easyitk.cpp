31f9c0c5d816558b4dcc18ab7ca3b998574cf7bb
Felipe Gustavo Bombardelli <fgbombardelli@c3sl.ufpr.br>
2015-06-22 20:22:28 -0300

    99%

--- easyitk.cpp
+++ easyitk.cpp
@@ -12,9 +12,19 @@
 #include <itkBinaryBallStructuringElement.h>
 #include <itkLineIterator.h>
 #include <itkLineConstIterator.h>
+#include <itkRegionOfInterestImageFilter.h>
+#include <itkNeighborhoodOperatorImageFilter.h>
+#include <itkSobelOperator.h>
+#include <itkAdaptiveHistogramEqualizationImageFilter.h>
+
+
+#include <itkOtsuThresholdImageFilter.h>
+
 
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
 
 
 using namespace std;
@@ -26,7 +36,8 @@ typedef itk::Image<unsigned char,2> Mat;
 typedef itk::Image<float,2>         MatFloat;
 typedef itk::CastImageFilter< MatFloat, Mat > CastFilterType;
 typedef itk::BinaryBallStructuringElement<unsigned char,2> StructuringElementType;
-
+typedef itk::NeighborhoodOperatorImageFilter<Mat, MatFloat> NeighborhoodOperatorImageFilterType;
+	
 
 #define uchar unsigned char
 
@@ -51,6 +62,13 @@ void image_set(Mat& input, int y, int x, uchar value){
 }
 
 
+float image_get(MatFloat& input, int y, int x){
+	Mat::IndexType pt;
+	pt[0] = x;
+	pt[1] = y;
+	return input.GetPixel(pt);
+}
+
 
 
 void image_size(int& rows, int& cols, Mat& image){
@@ -60,6 +78,18 @@ void image_size(int& rows, int& cols, Mat& image){
 }
 
 
+
+class Rect{
+  public:
+	int x,y,sx,sy;
+
+	Rect(int y,int x,int sy,int sx){
+		this->x = x;
+		this->y = y;
+		this->sx = sx;
+		this->sy = sy;
+	}
+};
 
 
 
@@ -73,6 +103,10 @@ class MatSystem {
 
 
 	itk::SobelEdgeDetectionImageFilter<Mat,MatFloat>::Pointer         sobel;
+
+	NeighborhoodOperatorImageFilterType::Pointer sobel1;
+	NeighborhoodOperatorImageFilterType::Pointer sobel2;
+
 	itk::CannyEdgeDetectionImageFilter<MatFloat, MatFloat>::Pointer   canny;
 
 	itk::ContourExtractor2DImageFilter<Mat>::Pointer      contour;
@@ -81,8 +115,14 @@ class MatSystem {
 	itk::BinomialBlurImageFilter<Mat,Mat>::Pointer  blur;
 	itk::BinaryThresholdImageFilter <Mat,Mat>::Pointer threshold;
 
+	itk::OtsuThresholdImageFilter<Mat,Mat>::Pointer otsu;
+
 	itk::BinaryErodeImageFilter <Mat,Mat, StructuringElementType>::Pointer erode;
 
+
+	itk::RegionOfInterestImageFilter<Mat,Mat>::Pointer roi;
+
+	itk::AdaptiveHistogramEqualizationImageFilter<Mat>::Pointer equalize;
 
 	CastFilterType::Pointer castfilter;
 
@@ -94,7 +134,7 @@ class MatSystem {
 		writerfloat = itk::ImageFileWriter<MatFloat>::New();
 		sobel       = itk::SobelEdgeDetectionImageFilter <Mat, MatFloat>::New();
 		castfilter  = CastFilterType::New();
-		//canny       = itk::CannyEdgeDetectionImageFilter<Mat, Mat>::New();
+		canny       = itk::CannyEdgeDetectionImageFilter<MatFloat, MatFloat>::New();
 		contour     = itk::ContourExtractor2DImageFilter<Mat>::New();
 
 
@@ -102,7 +142,16 @@ class MatSystem {
 		blur      = itk::BinomialBlurImageFilter<Mat,Mat>::New();
 		threshold = itk::BinaryThresholdImageFilter <Mat,Mat>::New();
 
-		erode     = itk::BinaryErodeImageFilter <Mat,Mat,StructuringElementType>::New();		
+		erode     = itk::BinaryErodeImageFilter <Mat,Mat,StructuringElementType>::New();
+
+		roi       = itk::RegionOfInterestImageFilter<Mat,Mat>::New();
+
+		sobel1    = NeighborhoodOperatorImageFilterType::New();
+		sobel2    = NeighborhoodOperatorImageFilterType::New();
+
+		otsu      = itk::OtsuThresholdImageFilter <Mat,Mat>::New();
+
+		equalize  = itk::AdaptiveHistogramEqualizationImageFilter<Mat>::New();
 	}
 
 
@@ -114,15 +163,22 @@ class MatSystem {
 
 
 	MatFloat& exec_sobel(Mat& input);
-	Mat&      exec_canny(Mat& input);
-	void      exec_extractContour(Mat& input);
+	MatFloat& exec_canny(MatFloat& input);
+	void      exec_extractContour(vector<Rect>& output, Mat& input);
 	Mat&      exec_blur(Mat& input);
 	Mat&      exec_threshold(Mat& input, int lower, int upper);
 	Mat&      exec_erode(Mat& input);
 
+	Mat&      exec_thresholdOtsu(Mat& input);
 
+	Mat&      get_roi(Mat& input, Rect rect);
 
 	void draw_Rect(Mat& input, int y, int x, int sy, int sx);
+
+	Mat& exec_equalize(Mat& input);
+
+	MatFloat& exec_sobelX(Mat& input);
+	MatFloat& exec_sobelY(Mat& input);
 };
 
 
@@ -169,17 +225,17 @@ Mat& MatSystem::resize(Mat& image, int new_rows, int new_cols){
 }
 
 
-MatFloat& MatSystem::exec_sobel(Mat& input){
+MatFloat& MatSystem::exec_sobel(Mat& input ){
 	sobel->SetInput(&input);
 	sobel->Update();
 	return *sobel->GetOutput();
 }
 
 
-Mat& MatSystem::exec_canny(Mat& input){
-	//canny->SetInput(&input);
-	//canny->Update();
-	//return *canny->GetOutput();
+MatFloat& MatSystem::exec_canny(MatFloat& input){
+	canny->SetInput(&input);
+	canny->Update();
+	return *canny->GetOutput();
 
 
 	//ms.cannySegmentation->SetInput( reader2->GetOutput() );
@@ -203,7 +259,8 @@ void MatSystem::draw_Rect(Mat& input, int y, int x, int sy, int sx){
 	}
 }
 
-void MatSystem::exec_extractContour(Mat& input){
+void MatSystem::exec_extractContour(vector<Rect>& output, Mat& input){
+	output.clear();
 	contour->SetInput(&input);
 	contour->SetContourValue(25);
 	contour->Update();
@@ -244,19 +301,21 @@ void MatSystem::exec_extractContour(Mat& input){
 			}
 			//cout << endl;
 
-			cout << media << endl;
+			//cout << media << endl;
 
-			this->draw_Rect(input, min_y, min_x, dy, dx);
-			if ( media > 290 && media < 320 )
-				cout << "  5 centavos\n";
-			else if ( media > 270 && media < 290 )
-				cout << " 10 centavos\n";
-			else if ( media > 340 && media < 360 )
-				cout << " 25 centavos\n";
-			else if ( media > 320 && media < 340 )
-				cout << " 50 centavos\n";
-			else if ( media > 370 && media < 400 )
-				cout << "100 centavos\n";
+			output.push_back( Rect(min_y,min_x,dy,dx) );
+			//this->draw_Rect(input, min_y, min_x, dy, dx);
+			if ( media >= 300 && media <= 325 )
+				cout << "  5 centavos - ";
+			else if ( media > 263 && media <= 299 )
+				cout << " 10 centavos - ";
+			else if ( media > 340 && media <=376 )
+				cout << " 25 centavos - ";
+			else if ( media>=326 && media <= 340 )
+				cout << " 50 centavos - ";
+			else if ( media >= 377 && media < 410 )
+				cout << "100 centavos - ";
+			cout << media << endl;
 		}
 
 	}
@@ -299,13 +358,63 @@ Mat& MatSystem::exec_erode(Mat& input){
 }
 
 
+Mat&  MatSystem::exec_thresholdOtsu(Mat& input){
+	otsu->SetInput(&input);
+	otsu->Update();
+	return *otsu->GetOutput();
+}
+
+
+
+Mat& MatSystem::get_roi(Mat& input, Rect rect){
+	Mat::IndexType start;
+	start[0] = rect.x;
+	start[1] = rect.y;
+
+	Mat::SizeType size;
+	size[0] = rect.sx;
+	size[1] = rect.sy;
+
+	Mat::RegionType desiredRegion;
+	desiredRegion.SetSize(size);
+	desiredRegion.SetIndex(start);
+
+	roi->SetRegionOfInterest(desiredRegion);
+	roi->SetInput(&input);
+	roi->Update();
+
+	return *roi->GetOutput();
+}
 
 
 
 
+typedef itk::SobelOperator<float, 2> SobelOperatorType;
+
+MatFloat& MatSystem::exec_sobelX(Mat& input){
+	SobelOperatorType sobelOperator;
+	itk::Size<2> radius;
+	radius.Fill(1);
+	sobelOperator.SetDirection(0);
+	sobelOperator.CreateToRadius(radius);
+	sobel1->SetOperator(sobelOperator);
+	sobel1->SetInput(&input);
+	sobel1->Update();
+	return *sobel1->GetOutput();
+}
 
 
-
+MatFloat& MatSystem::exec_sobelY(Mat& input){
+	SobelOperatorType sobelOperator;
+	itk::Size<2> radius;
+	radius.Fill(1);
+	sobelOperator.SetDirection(1);
+	sobelOperator.CreateToRadius(radius);
+	sobel2->SetOperator(sobelOperator);
+	sobel2->SetInput(&input);
+	sobel2->Update();
+	return *sobel2->GetOutput();
+}
 
 
 
@@ -326,11 +435,18 @@ string getName(string url){
 }
 
 
+Mat& MatSystem::exec_equalize(Mat& input){
+	equalize->SetInput(&input);
+	equalize->SetRadius(1);
+	equalize->Update();
+	return *equalize->GetOutput();
+}
+
 
 
 int main(int argc, char** argv){
 
-
+	system("mkdir -p tmp");
 
 	int rows, cols;
 	for (int i=1; i<argc; i++){
@@ -348,19 +464,33 @@ int main(int argc, char** argv){
 
 		Mat& borrado = ms.exec_blur(img);
 
-		Mat& border  = ms.exec_threshold(borrado,100,255);
+		//Mat& canelson = ms.exec_canny(borrado);
 
 
-		Mat& menor = ms.exec_erode(border);
+		//Mat& equalized = ms.exec_equalize(borrado);
+		Mat& border  = ms.exec_threshold(borrado,120,255);
 
-		//MatFloat& sobel = ms.exec_sobel(img);
+		//Mat& border  = ms.exec_thresholdOtsu(borrado);
 
-		ms.exec_extractContour(menor);
-	
+		
+		//Mat& menor = ms.exec_erode(border);
+
+
+
+
+
+		vector<Rect> rectpkg;
+		ms.exec_extractContour(rectpkg, border);
+		for (int j=0; j<rectpkg.size(); j++){
+			ms.draw_Rect(border, rectpkg[j].y, rectpkg[j].x, rectpkg[j].sy, rectpkg[j].sx);
+		}
+		
+
+		ms.imwrite(string("tmp/")+getName(argv[i]), border);
 		//Mat& menor = ms.resize(img, 50, 50);
 
 
-		ms.imwrite(string("tmp/")+getName(argv[i]), menor);
+		//ms.imwrite(string("tmp/")+getName(argv[i]), menor);
 	}
 	return 0;
 }
@@ -370,6 +500,52 @@ int main(int argc, char** argv){
 
 
 
+/*for (int j=0; j<rectpkg.size(); j++){
+			Mat& coin = ms.get_roi(img,rectpkg[j]);
+			ms.imwrite(getName("a.jpg"), coin);
+
+			MatFloat& coin_border_x = ms.exec_sobelX(coin);
+			MatFloat& coin_border_y = ms.exec_sobelY(coin);
+
+			int border_rows,border_cols;
+			image_size(border_rows,border_cols,coin);
+
+
+			int counter[8];
+			for (int i=0; i<8; i++)
+				counter[i] = 0;
+
+			double t_tx=0.0, t_ty=0.0;
+
+			for (int iy=0; iy<border_rows; iy++){
+				for (int ix=0; ix<border_cols; ix++){
+					float th = 0;
+					float ay = image_get(coin_border_y,iy,ix);
+					float ax = image_get(coin_border_x,iy,ix);
+					th = atan2(ay,ax);
+					int direction =   ( (th*180.0/3.141592)/45 ) + 3;
+					counter[direction] += fabs(ay) + fabs(ax);
+
+
+
+
+
+					if ( fabs(ax) + fabs(ay) > 100 ){
+						t_tx += ax;
+						t_ty += ay;
+					}
+					
+				}
+			}
+
+			double desc[8];
+			double total = border_cols*border_rows;
+			for (int i=0; i<8; i++){
+				desc[i] = counter[i]/total;
+				cout << desc[i] << endl;
+			}
+
+		}*/
 
 
 
